c9d6a3429850455902822b4c3dd27197af4efe17
xenklao <109021940+xenklao@users.noreply.github.com>
2022-07-12 15:30:58 +0500

    Initial commit1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Connection connection = Util.getConnection();
+//      private final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
