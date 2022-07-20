19b5d1c59f6c3b95c8518d088596566dfca01816
xenklao <109021940+xenklao@users.noreply.github.com>
2022-07-21 00:24:23 +0500

    delete unusedimport

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,10 +3,10 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 
 import java.sql.SQLException;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class Main {
